bef253b3b670137800a714327f34aabd066e7449
19742021 <2458807275@qq.com>
2025-12-22 23:08:28 +0800

    更新bumblechat.sh脚本

--- scripts/bumblechat.sh
+++ scripts/bumblechat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES="0,1" python src/api.py \
     --yaml_config ./configs/inference/bumblechat.yaml \
-    --model_path /data/wangxinhao/bumblecore/checkpoints/sft/bumblebee_1.5b_Instruct/checkpoint-1000 \
+    --model_path <your model path> \
     --device_map cpu
